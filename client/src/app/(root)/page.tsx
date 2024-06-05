dbcc11ce0bbda78f3764ec0a68fc5cc853b7045a
amadich <jeridi.moha@gmail.com>
2024-06-05 20:01:23 +0100

    BURGERANIME Sally Edition v:0.6 😔

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -1,6 +1,5 @@
 import DescMainScene from "@/components/DescMainScene";
 import MainScene from "@/components/MainScene";
-import Template from "../template";
 
 // Date: 03/08/21
 export default function Home() {
